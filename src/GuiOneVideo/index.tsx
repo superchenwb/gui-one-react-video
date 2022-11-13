74204bff8fb546382bb81c4ecdf0aaf05ffc616e
chen <chen1990wb@163.com>
2022-11-13 10:35:12 +0800

    fix: 添加className

--- src/GuiOneVideo/index.tsx
+++ src/GuiOneVideo/index.tsx
@@ -3,13 +3,14 @@ import 'video-react/dist/video-react.css';
 import { Player, BigPlayButton } from 'video-react';
 
 export interface GuiOneVideoProps {
+  className?: string;
   src: string;
   poster?: string;
 }
 
-export const GuiOneVideo = ({ src, poster }: GuiOneVideoProps) => {
+export const GuiOneVideo = ({ className, src, poster }: GuiOneVideoProps) => {
   return (
-    <Player poster={poster} src={src}>
+    <Player className={className} poster={poster} src={src}>
       <BigPlayButton position="center" />
     </Player>
   );
